534d2a4c4cb84fcbbfdac59d43eec99b30becefc
Gus-G751 <gustavo.vy@gmail.com>
2026-07-09 23:47:19 +0200

    Added results from UPM psicolinguistics LLM

--- iconicity_english/output/correlations.py
+++ iconicity_english/output/correlations.py
@@ -10,15 +10,14 @@ method = 'pearson'  # method correlation
 print(f"Using method: {method}")
 
 # Read data
-df = pd.read_excel('test_split_joined.xlsx')
+df = pd.read_excel('iconicity_ratings_english_joined.xlsx')
 
 # Select iconicity columns
-iconicity_cols = ['rating', 'iconicity_new']
+iconicity_cols = ['rating', 'iconicity_english_base_prompt_gpt-4o', 'iconicity_english_base_prompt_gpt-4o_finetuned', 'iconicity_english_UPM-LLM']
 df_iconicity = df[iconicity_cols].apply(pd.to_numeric, errors='coerce')
-df_iconicity.columns = ['human',  'test_dataset']
 
 # Correlations
 corr = df_iconicity.corr(method=method)
 
 print(corr)
-corr.to_excel("correlations_test_dataset.xlsx", index=False)
+corr.to_excel("correlations_upm-llm.xlsx", index=False)
